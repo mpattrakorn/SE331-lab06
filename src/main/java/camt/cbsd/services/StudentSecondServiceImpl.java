839c08adba5314d715cca3b88f4390aeb7d38b5d
DESKTOP-9CKSS1Q\CAMT <mark29162@gmail.com>
2017-03-24 16:16:06 +0700

    lab6

--- src/main/java/camt/cbsd/services/StudentSecondServiceImpl.java
+++ src/main/java/camt/cbsd/services/StudentSecondServiceImpl.java
@@ -1,6 +1,9 @@
 package camt.cbsd.services;
 
+import camt.cbsd.dao.StudentDao;
 import camt.cbsd.entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
@@ -8,17 +11,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Profile("secondDataSource")
+@ConfigurationProperties(prefix = "server")
 @Service
 public class StudentSecondServiceImpl implements StudentService {
-    @Override
-    public List<Student> getStudents() {
-        List<Student> students = new ArrayList<>();
 
-        Student student = new Student(1,"SE-000","Chartchai","D",
-                5.00,"images/temp.gif",true,0,
-                "The Special One");
-        students.add(student);
+    @Autowired
+    StudentDao studentDao;
 
-        return students;
+    public List<Student> getStudents(){
+
+        return studentDao.getStudents();
     }
 }
